0985d9ca06106a64688f22582f14337805de9658
emerkaspar <emerkaspar@gmail.com>
2025-09-20 22:45:59 -0300

    Descrição da alteração

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCplzRR5imGdGCpr6PnKEwo2rs5qPrV6c",
+    authDomain: "rianalise.firebaseapp.com",
+    projectId: "rianalise",
+    storageBucket: "rianalise.firebasestorage.app",
+    messagingSenderId: "46433102386",
+    appId: "1:46433102386:web:918bfbc2f0834604daaa2b",
+    measurementId: "G-ZQ2178VY8N"
+};
